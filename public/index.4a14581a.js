0bc51b8ee7f5ad8f15b8faf7c0ae1c6dbe93897e
markus-langgeng <markus.langgeng4@gmail.com>
2021-12-24 08:01:24 +0700

    tes npm run build dan dev

---
+++ public/index.4a14581a.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementsByClassName("toggle-btn")[0],t=document.getElementsByClassName("nav-links-container")[0],n=document.getElementsByClassName("nav-anchor");e.addEventListener("click",(()=>{t.classList.toggle("active")}));for(var s=0;s<n.length;s++)n[s].addEventListener("click",(()=>{t.classList.remove("active")}))}();
+//# sourceMappingURL=index.4a14581a.js.map
