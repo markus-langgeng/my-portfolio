684dd754566be38c7e15f5d1a656cf4426e3482b
markus-langgeng <markus.langgeng4@gmail.com>
2022-01-14 11:23:09 +0700

    memperbaiki navbar, hamburger menu

--- dist/script.41b293a4.js
+++ dist/script.41b293a4.js
@@ -474,11 +474,41 @@ function toggleNavBar() {
     const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
     const navLinks = document.getElementsByClassName("nav-links-container")[0];
     const navLinksAnchor = document.getElementsByClassName("nav-anchor");
+    //menghapus class active dan mengubah display ke non kalau
+    //nav-link-containernya masih kebuka trus screen nya jadi lebar
+    //klo pake display block sama none, ga bisa pake transition
+    //klo pake visibility bisa pake transition & menyembunyikan teks dari screen reader
+    const smallBp = window.matchMedia("(min-width: 40em)");
+    function displayBlock(x) {
+        if (!(x.matches && navLinks.classList.contains("active"))) return;
+        else {
+            navLinks.classList.remove("active");
+            // navLinks.style.display = "none";
+            navLinks.style.visibility = "hidden";
+            console.log("x matches");
+        }
+    }
+    smallBp.addEventListener("change", displayBlock);
+    displayBlock(smallBp);
+    //toggle ham menu
     toggleBtn.addEventListener("click", ()=>{
-        navLinks.classList.toggle("active");
+        if (navLinks.classList.contains("active")) {
+            navLinks.classList.remove("active");
+            // navLinks.style.display = "none";
+            navLinks.style.visibility = "hidden";
+            toggleBtn.setAttribute("aria-expandend", "false");
+        } else {
+            navLinks.classList.add("active");
+            // navLinks.style.display = "block";
+            navLinks.style.visibility = "visible";
+            toggleBtn.setAttribute("aria-expandend", "true");
+        }
     });
+    //tutup ham menu klo link di klik
     for(var i = 0; i < navLinksAnchor.length; i++)navLinksAnchor[i].addEventListener("click", ()=>{
         navLinks.classList.remove("active");
+        // navLinks.style.display = "none";
+        navLinks.style.visibility = "hidden";
     });
 }
 exports.default = toggleNavBar;
@@ -553,4 +583,4 @@ exports.default = navbarObserver;
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}]},["dpcih","iWSTb"], "iWSTb", "parcelRequire82ef")
 
-//# sourceMappingURL=index.41b293a4.js.map
+//# sourceMappingURL=script.41b293a4.js.map
--- src/js/ham-menu.js
+++ src/js/ham-menu.js
@@ -2,15 +2,46 @@ export default function toggleNavBar() {
     const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
     const navLinks = document.getElementsByClassName("nav-links-container")[0];
     const navLinksAnchor = document.getElementsByClassName("nav-anchor");
-    
-    toggleBtn.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-    })
 
-    for (var i=0; i < navLinksAnchor.length; i++) {
+    //menghapus class active dan mengubah display ke non kalau
+    //nav-link-containernya masih kebuka trus screen nya jadi lebar
+    //klo pake display block sama none, ga bisa pake transition
+    //klo pake visibility bisa pake transition & menyembunyikan teks dari screen reader
+    const smallBp = window.matchMedia("(min-width: 40em)");
+    function displayBlock(x) {
+        if (!(x.matches && navLinks.classList.contains("active"))) {
+            return;
+        } else {
+            navLinks.classList.remove("active");
+            // navLinks.style.display = "none";
+            navLinks.style.visibility = "hidden";
+            console.log("x matches");
+        }
+    }
+    smallBp.addEventListener("change", displayBlock);
+    displayBlock(smallBp);
+
+    //toggle ham menu
+    toggleBtn.addEventListener("click", () => {
+        if (navLinks.classList.contains("active")) {
+            navLinks.classList.remove("active");
+            // navLinks.style.display = "none";
+            navLinks.style.visibility = "hidden";
+            toggleBtn.setAttribute("aria-expandend", "false");
+        } else {
+            navLinks.classList.add("active");
+            // navLinks.style.display = "block";
+            navLinks.style.visibility = "visible";
+            toggleBtn.setAttribute("aria-expandend", "true");
+        }
+    });
+
+    //tutup ham menu klo link di klik
+    for (var i = 0; i < navLinksAnchor.length; i++) {
         navLinksAnchor[i].addEventListener("click", () => {
             navLinks.classList.remove("active");
-        })
+            // navLinks.style.display = "none";
+            navLinks.style.visibility = "hidden";
+        });
     }
-    
 }
